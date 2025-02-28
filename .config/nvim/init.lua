46ff7b672d7312987e44dfee85104cd67c08f8cf
koromaki88 <officialngannhien@gmail.com>
2025-02-28 17:06:12 +0700

    re-add tab format

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,6 +11,12 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = "a"
 
+-- Change TAB indentation length
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
