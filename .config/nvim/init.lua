6d14c8e29e02de3af657a498cf923f07367971db
koromaki88 <officialngannhien@gmail.com>
2025-06-03 14:49:59 +0700

    cmake-language-server lsp nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -444,6 +444,7 @@ require("lazy").setup({
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				clangd = {},
+				cmake = {},
 				ts_ls = {},
 				cssls = {},
 				html = {},
