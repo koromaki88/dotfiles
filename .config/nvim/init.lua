41732d08471df4d59415d20a989ef166a1244f7d
koromaki88 <officialngannhien@gmail.com>
2025-07-13 03:52:49 +0700

    add toggle hotkey for line-wrapping

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -75,6 +75,9 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Toggle keybinds
+vim.keymap.set("n", "<leader>tw", ":set wrap!<CR>", { desc = "Toggle line-wrapping" })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
