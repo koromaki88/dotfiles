0fe9bb6b708ae9ddfa857f74efacada880f62ea6
koromaki88 <officialngannhien@gmail.com>
2025-07-30 05:45:00 +0700

    add asm_lsp for neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -476,14 +476,15 @@ require("lazy").setup({
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
+				asm_lsp = {},
+				basedpyright = {},
 				bashls = {},
 				clangd = {},
 				cmake = {},
-				ts_ls = {},
 				cssls = {},
 				html = {},
-				basedpyright = {},
 				tailwindcss = {},
+				ts_ls = {},
 
 				-- gopls = {},
 				-- pyright = {},
