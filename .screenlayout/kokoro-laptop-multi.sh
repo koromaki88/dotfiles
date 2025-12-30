c118ab712aa9ece2fc3f8d3d7b445953e8e8c379
koromaki88 <officialngannhien@gmail.com>
2025-12-30 19:57:41 +0700

    refill wallpaper multi screen laptop script

--- .screenlayout/kokoro-laptop-multi.sh
+++ .screenlayout/kokoro-laptop-multi.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 xrandr --output eDP-1 --primary --mode 2880x1800 --pos 2560x0 --rotate normal --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
+feh --bg-fill "/home/kokoro/dotfiles/img/31A.png"
