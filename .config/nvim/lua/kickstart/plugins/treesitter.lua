40507224e0a5af363d3d4deefe8e8a577162a459
koromaki88 <officialngannhien@gmail.com>
2026-04-07 21:51:15 +0700

    nvim(fix): update treesitter to fix incompatibility with nvim 0.12

--- .config/nvim/lua/kickstart/plugins/treesitter.lua
+++ .config/nvim/lua/kickstart/plugins/treesitter.lua
@@ -2,9 +2,10 @@ return {
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		main = "nvim-treesitter.configs", -- Sets main module to use for opts
-		opts = {
-			ensure_installed = {
+		main = "nvim-treesitter", -- Sets main module to use for opts
+		opts = {},
+		init = function()
+			local ensureInstalled = {
 				"bash",
 				"c",
 				"html",
@@ -14,17 +15,24 @@ return {
 				"markdown_inline",
 				"vim",
 				"vimdoc",
-			},
-			-- Autoinstall languages that are not installed
-			auto_install = true,
-			highlight = {
-				enable = true,
-				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-				--  If you are experiencing weird indenting issues, add the language to
-				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-				additional_vim_regex_highlighting = { "ruby", "html" },
-			},
-			indent = { enable = true, disable = { "ruby", "html" } },
-		},
+			}
+			local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+			local parsersToInstall = vim.iter(ensureInstalled)
+				:filter(function(parser)
+					return not vim.tbl_contains(alreadyInstalled, parser)
+				end)
+				:totable()
+			require("nvim-treesitter").install(parsersToInstall)
+		end,
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					-- Enable treesitter highlighting and disable regex syntax
+					pcall(vim.treesitter.start)
+					-- Enable treesitter-based indentation
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+		end,
 	},
 }
