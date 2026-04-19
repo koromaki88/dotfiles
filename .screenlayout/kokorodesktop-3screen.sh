060cc84d2ff293b029c783a052327dcc7e93767a
koromaki88 <officialngannhien@gmail.com>
2026-04-20 02:30:38 +0700

    setup(feat): xrandr script for 3 screens, switch feh -> xwallpaper

---
+++ .screenlayout/kokorodesktop-3screen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-0 --primary --mode 2560x1440 --pos 1080x240 --rotate normal --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 3640x1310 --rotate normal --output DP-2 --mode 1920x1080 --rate 120 --pos 0x0 --rotate left --output DP-3 --off --output DP-4 --off --output DP-5 --off
